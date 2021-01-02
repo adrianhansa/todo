c634bf798207ba97104e0bfe712e7ef915a631b4
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-01-02 07:19:44 +0000

    Add jps templating engine

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+app.set('view engine','ejs')
 
-const views = path.join(__dirname, './src/views')
 const public = path.join(__dirname, './public')
 
 app.use(express.static(public))
 
 app.get('/', (req, res)=>{
-    res.sendFile(public+'/index.html')
+    res.render('index')
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about')
 })
 
 app.use((req,res)=>{
-    res.sendFile(views+'/404.html')
+    res.render('404.html')
 })
 
 app.listen(process.env.PORT || 3000,()=>{
