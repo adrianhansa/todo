a3198f88c0260867032d14e862ecb6520080c06d
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-01-03 15:02:24 +0000

    add mongodb connection url and install moongoose, preparing the connection to db

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 
 const app = express()
+
+//connect to mongodb
+const dbURI = "mongodb+srv://adrianhansa:Stroiana1916@cluster0.uzvpc.mongodb.net/todo-app?retryWrites=true&w=majority"
+mongoose.connect(dbURI, {useNewUrlParser:true,useUnifiedTopology:true})
+        .then(result=>{
+            app.listen(process.env.PORT || 3000,()=>{
+                console.log("Server listening for requests at port 3000")
+            })
+        })
+        .catch(error=>console.log(error))
+
 app.set('view engine','ejs')
 
 const public = path.join(__dirname, './public')
@@ -23,8 +35,4 @@ app.get('/about',(req,res)=>{
 
 app.use((req,res)=>{
     res.status(404).render('404',{title:"Page not found"})
-})
-
-app.listen(process.env.PORT || 3000,()=>{
-    console.log("Server listening for requests at port 3000")
 })
