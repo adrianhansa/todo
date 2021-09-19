fdf51d0161e5c94b36e22bf5f30c93de902d5418
Adrian Hansa <adrian.hansa@yahoo.com>
2021-09-19 16:22:07 +0100

    Added FlatList to render the items stored in the component state.

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Text,
+  Button,
+  StyleSheet,
+  TextInput,
+  View,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = (task) => {
+    const todo = { id: Math.random(), task: task };
+    setTodos([...todos, todo]);
+    setTodo("");
+    console.log(todos);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        placeholder="Enter a todo"
+        style={styles.inputText}
+        value={todo}
+        onChangeText={(e) => setTodo(e)}
+      />
+      <Button title="Add Todo" onPress={() => addTodo(todo)} />
+      <FlatList
+        data={todos}
+        renderItem={({ item }) => {
+          return <Text>{item.task}</Text>;
+        }}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
@@ -14,8 +40,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputText: {
+    padding: 10,
+    borderColor: "#000000",
+    marginBottom: 10,
+    borderWidth: 1,
   },
 });
